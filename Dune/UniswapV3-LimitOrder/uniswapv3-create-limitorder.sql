ae0a17631bf5cd8c160cf87470fa7826061e199e
stasi <stasi009@163.com>
2024-02-12 06:49:29 +0800

    save

--- Dune/UniswapV3-LimitOrder/uniswapv3-create-limitorder.sql
+++ Dune/UniswapV3-LimitOrder/uniswapv3-create-limitorder.sql
@@ -1,54 +1,78 @@
 
-with pair_add_liquidity as (
-    -- aim of this CTE is to find pair information
-    select
-        em.evt_tx_hash, 
-        -- owner is useless, most of the time, owner is NftPositionManager, it's not the real owner
-        em.contract_address as pair_addr,
-        pc.token0,
-        tk0.symbol as tk0symbol,
-        (em.amount0 / power(10, tk0.decimals)) as amt0_float,
-        pc.token1,
-        tk1.symbol as tk1symbol,
-        (em.amount1 / power(10, tk1.decimals)) as amt1_float,
-        em.tickLower,
-        em.tickUpper
-    from uniswap_v3_ethereum.Pair_evt_Mint em
-    inner join uniswap_v3_ethereum.Factory_evt_PoolCreated pc
-        on pc.pool = em.contract_address
+/*
+! why use 'function call' table and parse the input parameters
+other than using uniswap_v3_ethereum.Pair_evt_Mint ??
+because Pair_evt_Mint doesn't have tokenId
+and Pair_evt_Mint's owner is always NftPositionManager, not the real owner I want
+*/
+with nft_tokens as (
+    with call_mint as (
+        select 
+            output_tokenId as tokenId
+            -- from_hex cast string to varbinary
+            , from_hex(json_extract_scalar(params_json,'$.token0')) as token0 
+            , from_hex(json_extract_scalar(params_json,'$.token1')) as token1 
+            -- ! NOTE: Pair_evt_Mint's owner is always NftPositionManager
+            -- this recipient is NFT's real owner
+            , from_hex(json_extract_scalar(params_json,'$.recipient')) as liquid_provider 
+            , cast(json_extract_scalar(params_json,'$.tickLower') as int) as tickLower 
+            , cast(json_extract_scalar(params_json,'$.tickUpper') as int) as tickUpper 
+            -- call it 'fee_int', remind me, divide it by 1e6 to get real fee rate
+            , cast(json_extract_scalar(params_json,'$.fee') as int) as fee_int
+        from (
+            select 
+                m.*,
+                json_parse(params) as params_json
+            from uniswap_v3_ethereum.NonfungibleTokenPositionManager_call_mint m
+            where call_success
+                and call_block_time >= now() - interval '{{back_days}}' day
+        )
+    )
+    select 
+        cm.*,
+        (tk0.symbol || '-' || tk1.symbol) as pair_symbol,
+        coalesce(tk0.decimals,18) as tk0decimal,
+        coalesce(tk1.decimals,18) as tk1decimal
+    from call_mint cm
     inner join tokens.erc20 tk0 
-        on pc.token0 = tk0.contract_address
+        on cm.token0 = tk0.contract_address
         and tk0.blockchain = 'ethereum'
     inner join tokens.erc20 tk1 
-        on pc.token1 = tk1.contract_address
+        on cm.token1 = tk1.contract_address
         and tk1.blockchain = 'ethereum'
-    where 
-        (em.amount0 =0 or em.amount1=0)
-        and em.evt_block_time >= now() - interval '{{back_days}}' day
 ),
-nft_add_liquidity as (
-    -- aim of this CTE is to find tokenId
+prices as (
     select 
-        evt_tx_hash
-        , amount0
-        , amount1
-        , liquidity
-        , tokenId
-    from uniswap_v3_ethereum.NonfungibleTokenPositionManager_evt_IncreaseLiquidity
-    where 
-        (amount0=0 or amount1=0) -- add redundant constraints to reduce result size and speed up
-        and evt_block_time >= now() - interval '{{back_days}}' day
+        minute,
+        price,
+        contract_address
+    from prices.usd
+    where blockchain = 'ethereum'
+        and minute >= now() - interval '{{back_days}}' day
 ),
-nft_mint as (
-    
+add_single_liquidity as (
+    select 
+        il.evt_tx_hash as tx_hash
+        , il.evt_block_time as block_time
+        , nft.pair_symbol
+        , (il.amount0 / power(10, nft.tk0decimal)) as amt0_float
+        , (il.amount0 / power(10, nft.tk0decimal) * p0.price) as amt0_usd
+        , (il.amount1 / power(10, nft.tk1decimal)) as amt1_float
+        , (il.amount1 / power(10, nft.tk1decimal) * p1.price) as amt1_usd
+        , il.liquidity
+        , il.tokenId
+        , nft.liquid_provider
+    from uniswap_v3_ethereum.NonfungibleTokenPositionManager_evt_IncreaseLiquidity il
+    inner join nft_tokens nft 
+        on il.tokenId = nft.tokenId
+    inner join prices p0
+        on p0.contract_address = nft.token0
+        and p0.minute = date_trunc('minute',il.evt_block_time)
+    inner join prices p1
+        on p1.contract_address = nft.token1
+        and p1.minute = date_trunc('minute',il.evt_block_time)
+    where 
+        (il.amount0=0 or il.amount1=0) -- add redundant constraints to reduce result size and speed up
+        and il.evt_block_time >= now() - interval '{{back_days}}' day
 )
-
-select 
-    pal.*,
-    nal.liquidity,
-    nal.tokenId
-from pair_add_liquidity pal 
-inner join nft_add_liquidity nal-- use inner join, mismatch is impossible
-    -- TODO: assume one txn has only add liquidity once
-    -- so when two event hash match, it means the same 'add liquidity' operation
-    on pal.evt_tx_hash = nal.evt_tx_hash
+select * from add_single_liquidity
