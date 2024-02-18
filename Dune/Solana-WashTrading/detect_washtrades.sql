8b761abeb96d88f5da652075a508623f338e6aa5
stasi <stasi009@163.com>
2024-02-18 10:16:48 +0800

    save

--- Dune/Solana-WashTrading/detect_washtrades.sql
+++ Dune/Solana-WashTrading/detect_washtrades.sql
@@ -25,33 +25,28 @@ with latest_nft_trades as (
 
 same_buyer_seller as (
     select 
-        unique_tradeid,
-        'same_buyer_seller' as wash_type
+        trade_tx_index,
+        true as same_buyer_seller
     from latest_nft_trades t
     where buyer = seller 
 ),
 
 back_forth_trade as (
     select 
-        t1.unique_nft_id,
-        
-        t1.block_time as t1_blocktime,
-        t1.tx_id as t1_tx,
-        t1.buyer as t1_buyer,
-        t1.seller as t1_seller,
-        t1.amount_usd as t1_usd,
-
-        t2.block_time as t2_bl2cktime,
-        t2.tx_id as t2_tx,
-        t2.buyer as t2_buyer,
-        t2.seller as t2_seller,
-        t2.amount_usd as t2_usd
+        t1.trade_tx_index,
+        true as back_forth_trade
     from latest_nft_trades t1
     inner join latest_nft_trades t2
-    on t1.unique_nft_id = t2.unique_nft_id
-    and t1.buyer = t2.seller
-    and t1.seller = t2.buyer
-    and t2.block_time > t1.block_time
+        on t1.unique_nft_id = t2.unique_nft_id
+        and t1.trade_tx_index is not null
+        and t1.buyer = t2.seller
+        and t1.seller = t2.buyer
+    group by t1.trade_tx_index
 )
-select * from back_forth_trade
-order by unique_nft_id, t1_blocktime
+
+select 
+    nt.*
+from latest_nft_trades nt
+inner join back_forth_trade bft
+    on bft.trade_tx_index = nt.trade_tx_index
+order by nt.unique_nft_id, nt.block_time
