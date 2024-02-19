bcd096def89ceca928fbd6c861135cd27e6a2553
stasi <stasi009@163.com>
2024-02-19 11:27:57 +0800

    save

--- Dune/Solana-WashTrading/detect_washtrades.sql
+++ Dune/Solana-WashTrading/detect_washtrades.sql
@@ -1,26 +1,33 @@
 
 with latest_nft_trades as (
     select 
-        block_time,
-        tx_id || '-'|| cast(outer_instruction_index as varchar) || '-' || cast(COALESCE(inner_instruction_index,0) as varchar) as trade_tx_index,
+        temp.* 
+    from (
+        select 
+            block_time,
+            tx_id || '-'|| cast(outer_instruction_index as varchar) || '-' || cast(COALESCE(inner_instruction_index,0) as varchar) as trade_tx_index,
 
-        case 
-            when account_mint is not null then 'mint_' || account_mint
-            when account_merkle_tree is not null then 'merkle_' || account_merkle_tree || '_' ||cast(leaf_id as varchar)
-        end as unique_nft_id,
+            case 
+                when account_mint is not null then 'mint_' || account_mint
+                when account_merkle_tree is not null then 'merkle_' || account_merkle_tree || '_' ||cast(leaf_id as varchar)
+            end as unique_nft_id,
 
-        project,
-        version,
+            project,
+            version,
 
-        buyer,
-        seller,
+            buyer,
+            seller,
 
-        amount_usd,
-        taker_fee_amount_usd,
-        maker_fee_amount_usd,
-        royalty_fee_amount_usd
-    from nft_solana.trades
-    where block_time >= now() - interval '{{backdays}}' day
+            amount_usd,
+            taker_fee_amount_usd,
+            maker_fee_amount_usd,
+            royalty_fee_amount_usd
+        from nft_solana.trades
+    ) temp
+    where temp.block_time >= now() - interval '{{backdays}}' day
+        and temp.trade_tx_index is not null
+        and temp.unique_nft_id is not null
+        and amount_usd >=1 -- limit dataset size
 ),
 
 same_buyer_seller as (
@@ -38,7 +45,6 @@ back_forth_trade as (
     from latest_nft_trades t1
     inner join latest_nft_trades t2
         on t1.unique_nft_id = t2.unique_nft_id
-        and t1.trade_tx_index is not null
         and t1.buyer = t2.seller
         and t1.seller = t2.buyer
     group by t1.trade_tx_index
@@ -52,7 +58,6 @@ buy_same_3x as (
     inner join latest_nft_trades t2 
         on t1.unique_nft_id = t2.unique_nft_id
         and t1.buyer = t2.buyer
-        and t1.trade_tx_index is not null
     group by t1.trade_tx_index
     having count(t1.trade_tx_index) >= 3
 ),
@@ -65,17 +70,26 @@ sell_same_3x as (
     inner join latest_nft_trades t2 
         on t1.unique_nft_id = t2.unique_nft_id
         and t1.seller = t2.seller
-        and t1.trade_tx_index is not null
     group by t1.trade_tx_index
     having count(t1.trade_tx_index) >= 3
 )
 
 select 
-    nt.buyer,
-    nt.unique_nft_id,
-    count(*) as count
-from buy_same_3x b3
-inner join latest_nft_trades nt
-    on nt.trade_tx_index = b3.trade_tx_index
-group by 1,2
-order by count desc
+    lnt.*,
+    COALESCE(sbs.same_buyer_seller,false) as same_buyer_seller,
+    COALESCE(bft.back_forth_trade, false) as back_forth_trade,
+    COALESCE(bs3.buy_same_3x,false) as buy_same_3x,
+    COALESCE(ss3.sell_same_3x,false) as sell_same_3x,
+    COALESCE(sbs.same_buyer_seller
+            or bft.back_forth_trade
+            or bs3.buy_same_3x
+            or ss3.sell_same_3x, false) as is_wash_trade 
+from latest_nft_trades lnt 
+left join same_buyer_seller sbs 
+    on sbs.trade_tx_index = lnt.trade_tx_index
+left join back_forth_trade bft 
+    on bft.trade_tx_index = lnt.trade_tx_index
+left join buy_same_3x bs3 
+    on bs3.trade_tx_index = lnt.trade_tx_index
+left join sell_same_3x ss3 
+    on ss3.trade_tx_index = lnt.trade_tx_index
--- Dune/Solana-WashTrading/x_transfer_beside_trade.sql
+++ Dune/Solana-WashTrading/x_transfer_beside_trade.sql
@@ -1,52 +1,20 @@
-
-with nft_trade_pairs as (
+with transfers_outof_trades as (
+    with transfer_tx as (
+        select distinct tx_id 
+        from tokens_solana.transfers
+        where action = 'transfer'
+            and block_time >= now() - interval '{{backdays}}' day
+    ),
+    trade_tx as (
+        select distinct tx_id 
+        from nft_solana.trades
+        where block_time >= now() - interval '{{backdays}}' day
+    )
     select 
-        tx_id,
-        case 
-            when buyer > seller then buyer || '-' || seller
-            else seller || '-' || buyer 
-        end trade_pair
-    from nft_solana.trades
-    where block_time >= now() - interval '{{backdays}}' day
-),
-
-token_transfer_pairs as (
-    select 
-        tx_id,
-        case 
-            when from_owner > to_owner then from_owner || '-' || to_owner
-            else to_owner || '-' || from_owner 
-        end transfer_pair
-    from tokens_solana.transfers
-    where action = 'transfer'
-        and block_time >= now() - interval '{{backdays}}' day
+        f.tx_id as tx_id
+    from transfer_tx f   
+    left join trade_tx d 
+        on f.tx_id = d.tx_id -- transfer happen during trade
+    where d.tx_id is null -- transfer out of trade
 )
-
-select 
-    nt.trade_pair,
-    case 
-        when tf.transfer_pair is null then 0 
-        else 1
-    end as transfer_beside_trade
-from nft_trade_pairs nt
-left join token_transfer_pairs tf
-    on nt.trade_pair = tf.transfer_pair
-    and nt.tx_id <> tf.transfer_pair
-
--- select 
---     transfer_beside_trade,
---     count(trade_pair) as num_pairs
--- from (
---     select 
---         nt.trade_pair,
---         case 
---             when tf.transfer_pair is null then 0 
---             else 1 
---         end as transfer_beside_trade
---     from nft_trade_pairs nt
---     left join token_transfer_pairs tf
---         on nt.trade_pair = tf.transfer_pair
--- )
--- group by transfer_beside_trade
-
-
+select * from transfers_outof_trades
