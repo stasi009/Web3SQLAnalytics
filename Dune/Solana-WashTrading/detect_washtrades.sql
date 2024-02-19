4a49edccb60fbf3c0a6b8055d66139817d198792
stasi <stasi009@163.com>
2024-02-19 14:03:23 +0800

    save

--- Dune/Solana-WashTrading/detect_washtrades.sql
+++ Dune/Solana-WashTrading/detect_washtrades.sql
@@ -72,24 +72,53 @@ sell_same_3x as (
         and t1.seller = t2.seller
     group by t1.trade_tx_index
     having count(t1.trade_tx_index) >= 3
+),
+
+wash_extend_trades as (
+    select 
+        lnt.*,
+        cast(COALESCE(sbs.same_buyer_seller,false) as int) as same_buyer_seller,
+        cast(COALESCE(bft.back_forth_trade, false) as int) as back_forth_trade,
+        cast(COALESCE(bs3.buy_same_3x,false) as int) as buy_same_3x,
+        cast(COALESCE(ss3.sell_same_3x,false) as int) as sell_same_3x,
+        cast(COALESCE(sbs.same_buyer_seller
+                or bft.back_forth_trade
+                or bs3.buy_same_3x
+                or ss3.sell_same_3x, false) as int) as is_wash_trade 
+    from latest_nft_trades lnt 
+    left join same_buyer_seller sbs 
+        on sbs.trade_tx_index = lnt.trade_tx_index
+    left join back_forth_trade bft 
+        on bft.trade_tx_index = lnt.trade_tx_index
+    left join buy_same_3x bs3 
+        on bs3.trade_tx_index = lnt.trade_tx_index
+    left join sell_same_3x ss3 
+        on ss3.trade_tx_index = lnt.trade_tx_index
 )
 
+
 select 
-    lnt.*,
-    COALESCE(sbs.same_buyer_seller,false) as same_buyer_seller,
-    COALESCE(bft.back_forth_trade, false) as back_forth_trade,
-    COALESCE(bs3.buy_same_3x,false) as buy_same_3x,
-    COALESCE(ss3.sell_same_3x,false) as sell_same_3x,
-    COALESCE(sbs.same_buyer_seller
-            or bft.back_forth_trade
-            or bs3.buy_same_3x
-            or ss3.sell_same_3x, false) as is_wash_trade 
-from latest_nft_trades lnt 
-left join same_buyer_seller sbs 
-    on sbs.trade_tx_index = lnt.trade_tx_index
-left join back_forth_trade bft 
-    on bft.trade_tx_index = lnt.trade_tx_index
-left join buy_same_3x bs3 
-    on bs3.trade_tx_index = lnt.trade_tx_index
-left join sell_same_3x ss3 
-    on ss3.trade_tx_index = lnt.trade_tx_index
+    temp.*,
+    cast(temp.total_wash_num as double) / temp.total_num as wash_num_percent,
+    temp.total_wash_volume / temp.total_volume as wash_vol_percent,
+    temp.total_wash_taker_fee / temp.total_taker_fee as wash_takerfee_percent,
+    temp.total_wash_maker_fee / temp.total_maker_fee as wash_makerfee_percent,
+    temp.total_wash_royalty_fee / temp.total_royalty_fee as wash_royaltyfee_percent
+from (
+    select 
+        count(trade_tx_index) as total_num,
+        sum(is_wash_trade) as total_wash_num,
+
+        sum(amount_usd) as total_volume,
+        sum(amount_usd * is_wash_trade) as total_wash_volume,
+
+        sum(taker_fee_amount_usd) as total_taker_fee,
+        sum(taker_fee_amount_usd * is_wash_trade) as total_wash_taker_fee,
+
+        sum(maker_fee_amount_usd) as total_maker_fee,
+        sum(maker_fee_amount_usd * is_wash_trade) as total_wash_maker_fee,
+
+        sum(royalty_fee_amount_usd) as total_royalty_fee,
+        sum(royalty_fee_amount_usd * is_wash_trade) as total_wash_royalty_fee
+    from wash_extend_trades
+) temp
