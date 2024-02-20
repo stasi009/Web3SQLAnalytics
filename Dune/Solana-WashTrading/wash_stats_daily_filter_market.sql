9062842cc11940abfefd211707166c61789bd59f
stasi <stasi009@163.com>
2024-02-20 14:43:57 +0800

    save

--- Dune/Solana-WashTrading/wash_stats_daily_filter_market.sql
+++ Dune/Solana-WashTrading/wash_stats_daily_filter_market.sql
@@ -119,16 +119,37 @@ nft_trades_with_washflag as (
         on bsm.trade_tx_index = lnt.trade_tx_index
     left join sell_same_manytimes ssm
         on ssm.trade_tx_index = lnt.trade_tx_index
+),
+
+group_stats_by_day as (
+    select 
+        date_trunc('day',block_time) as block_day,
+
+        count(trade_tx_index) as total_num,
+        sum(is_wash_trade) as total_wash_num,
+
+        sum(amount_usd) as total_volume,
+        sum(amount_usd * is_wash_trade) as total_wash_volume,
+
+        sum(taker_fee_amount_usd) as total_taker_fee,
+        sum(taker_fee_amount_usd * is_wash_trade) as total_wash_taker_fee,
+
+        sum(maker_fee_amount_usd) as total_maker_fee,
+        sum(maker_fee_amount_usd * is_wash_trade) as total_wash_maker_fee,
+
+        sum(royalty_fee_amount_usd) as total_royalty_fee,
+        sum(royalty_fee_amount_usd * is_wash_trade) as total_wash_royalty_fee
+    from nft_trades_with_washflag
+    group by 1
 )
 
 select 
-    date_trunc('day',block_time) as block_day,
-    is_wash_trade,
-
-    count(trade_tx_index) as total_num,
-    sum(amount_usd) as total_volume,
-    sum(taker_fee_amount_usd) as total_taker_fee,
-    sum(maker_fee_amount_usd) as total_maker_fee,
-    sum(royalty_fee_amount_usd) as total_royalty_fee
-from nft_trades_with_washflag
-group by 1,2
+    temp.*,
+    temp.total_taker_fee / temp.total_volume as takerfee2vol_percent,
+    temp.total_maker_fee / temp.total_volume as makerfee2vol_percent,
+    cast(temp.total_wash_num as double) / temp.total_num as wash_num_percent,
+    temp.total_wash_volume / temp.total_volume as wash_vol_percent,
+    temp.total_wash_taker_fee / temp.total_taker_fee as wash_takerfee_percent,
+    temp.total_wash_maker_fee / temp.total_maker_fee as wash_makerfee_percent,
+    temp.total_wash_royalty_fee / temp.total_royalty_fee as wash_royaltyfee_percent
+from group_stats_by_day temp
