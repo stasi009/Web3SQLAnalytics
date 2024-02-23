7a36995b77880e90afaefb1b9f722d29aa65bbae
stasi <stasi009@163.com>
2024-02-23 13:48:30 +0800

    save

--- Flipside/UniswapV3-LiquidDistribution/temp1.sql
+++ Flipside/UniswapV3-LiquidDistribution/temp1.sql
@@ -1,4 +1,8 @@
 
+----------- sample parameters
+-- pool: 0x151CcB92bc1eD5c6D0F9Adb5ceC4763cEb66AC7f
+-- created at 2021-05-05
+-- fee tier 0.3%, tickSpacing =60
 with mint_burn as (
     select 
         tick_lower,
@@ -17,12 +21,20 @@ range_net_liquidity as (
     select 
         tick_lower,
         tick_upper,
-        sum(delta_liquidity) as range_liquidity
+        sum(delta_liquidity) as range_liq
     from mint_burn
     group by 1,2
     having sum(delta_liquidity) > 0
+),
+tick_net_liquidity as (
+    select  
+        ticks.value as tick,
+        sum(range_liq) as tick_net_liq
+    from range_net_liquidity as rl, 
+        lateral flatten(input => ARRAY_GENERATE_RANGE(tick_lower,tick_upper,{{step}})) as ticks
+    group by 1
 )
 
 select *   
-from range_net_liquidity
-order by tick_lower
+from tick_net_liquidity
+order by tick
