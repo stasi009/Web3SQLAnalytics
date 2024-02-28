f02491d89c7925340c4702985f3a16b75c33f6a3
stasi <stasi009@163.com>
2024-02-28 20:16:45 +0800

    save

--- Dune/Blockchain Comparison/solana_top_error_occur_accounts.sql
+++ Dune/Blockchain Comparison/solana_top_error_occur_accounts.sql
@@ -14,20 +14,39 @@ with account_n_success as (
         )
 )
 
-select 
-    account,
-    total_txns,
-    total_succ_txns,
-    cast(total_succ_txns as double) / total_txns as success_rate
-from (
+, high_fail_accounts as (
     select 
         account
-        , count(success) as total_txns
-        , count(success) filter (where success) as total_succ_txns
-    from account_n_success
-    group by 1
+        , total_txns
+        , total_succ_txns
+        , cast(total_succ_txns as double) / total_txns as success_rate
+    from (
+        select 
+            account
+            , count(success) as total_txns
+            , count(success) filter (where success) as total_succ_txns
+        from account_n_success
+        group by 1
+    )
+    where total_txns >= 100000
+        and cast(total_succ_txns as double) / total_txns < 0.5 -- only care about those high fail rate
 )
-where total_txns >= 500000
-    and cast(total_succ_txns as double) / total_txns < 0.5 -- only care about those high fail rate
-order by success_rate
 
+, program_names as (
+    select distinct  namespace,
+                    program_name,
+                    program_id
+    from solana.discriminators
+)
+
+select 
+    hfa.account
+    , pn.namespace
+    , pn.program_name
+    , hfa.total_txns
+    , hfa.total_succ_txns
+    , hfa.success_rate
+from high_fail_accounts hfa
+left join program_names pn 
+    on hfa.account = pn.program_id
+order by success_rate
