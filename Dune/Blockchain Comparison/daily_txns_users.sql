c5eff78db6eae1475f47d7b9a56e88de4b0a5e33
stasi <stasi009@163.com>
2024-02-28 16:23:12 +0800

    save

---
+++ Dune/Blockchain Comparison/daily_txns_users.sql
@@ -0,0 +1,24 @@
+
+with ethereum_daily_txns as (
+    select
+        block_date 
+        , num_txn
+        , num_success_txn
+        , cast(num_success_txn as double) / num_txn as txn_success_rate
+        , num_txn / 86400.0 as txn_per_sec
+        , num_success_txn / 86400.0 as succ_txn_per_sec
+        , num_users
+    from (
+        select 
+            block_date
+            , count(hash) as num_txn
+            , count(hash) filter (where success) as num_success_txn
+            , count(distinct "from") as num_users
+        from ethereum.transactions txn
+        -- between include both ends
+        where block_date between current_date - interval '{{back_days}}' day and current_date - interval '1' day
+        group by 1
+    )
+)
+
+select * from ethereum_daily_txns
