69d603ee9654909842fbb4ff37f5dfd6fd6c16c8
stasi <stasi009@163.com>
2024-02-29 10:14:38 +0800

    save

--- Dune/Blockchain Comparison/sub_evm_new_old_users.sql
+++ Dune/Blockchain Comparison/sub_evm_new_old_users.sql
@@ -3,43 +3,53 @@ with user_first_day as (
     select 
         "from" as user
         , min(block_date) as first_day
+    -- 这里不能添加时间限制，我们需要全局的初次交易时间，而非近期的第1次交易时间
     from {{evm_blockchain}}.transactions txn
     group by 1
 )
 
-, txn_grpby_day_usertype as (
+, txn_with_user_type as (
     select 
         txn.block_date
+        , txn.hash as txn_hash
+        , txn."from" as user
 
         , case  
-            when txn.block_date = fd.first_day then 'new'
-            else 'old' end 
-        as user_new_old
+            when txn.block_date = fd.first_day then true
+            else false 
+        end as is_new_user
 
-        , count(txn.hash) as num_txn
-        , count(txn.hash) filter (where success) as num_success_txn
-        , count(distinct txn."from") as num_users
     from {{evm_blockchain}}.transactions txn 
     inner join user_first_day fd -- I can inner join here because user_first_day derives from same dataset
         on txn."from" = fd.user
-        -- apply fiter condition during join, this can reduce join size
-        -- this can work, because I am using a inner join
-        and txn.block_date between current_date - interval '{{back_days}}' day and current_date - interval '1' day
-    group by 1,2
+    where 
+        txn.block_date between current_date - interval '{{back_days}}' day 
+                        and current_date - interval '1' day
+)
+
+, txn_grpby_day as (
+    select 
+        block_date
+
+        , count(txn_hash) filter (where is_new_user )as num_txn_new_users
+        , count(txn_hash) filter (where not is_new_user) as num_txn_old_users
+
+        , count(distinct user) filter (where is_new_user) as num_new_users
+        , count(distinct user) filter (where not is_new_user) as num_old_users
+    from txn_with_user_type
+    group by 1
 )
 
 select
     '{{evm_blockchain}}' as blockchain
     , block_date 
-    , user_new_old
 
-    , num_txn
-    , num_success_txn
-    , cast(num_success_txn as double) / num_txn as txn_success_rate
+    , num_txn_new_users
+    , num_txn_old_users
+    , cast(num_txn_new_users as double) / (num_txn_new_users + num_txn_old_users) as new_user_txn_percent
 
-    , num_txn / 86400.0 as txn_per_sec
-    , num_success_txn / 86400.0 as succ_txn_per_sec
-    
-    , num_users
-from txn_grpby_day_usertype
-order by block_date, user_new_old
+    , num_new_users
+    , num_old_users
+    , cast(num_new_users as double) / (num_new_users + num_old_users) as new_user_percent
+from txn_grpby_day
+order by block_date
