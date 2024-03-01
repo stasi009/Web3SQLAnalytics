7710979ce7643896165d389e70d1791bff8d1e2d
stasi <stasi009@163.com>
2024-03-01 16:46:39 +0800

    save

--- Dune/Blockchain Comparison/main_compare_fees.sql
+++ Dune/Blockchain Comparison/main_compare_fees.sql
@@ -50,6 +50,24 @@ with prices_usd as (
     )
 )
 
-, 
+, avalanche_c_daily_fee as (
+    select 
+        'avalanche_c' as blockchain
+        , block_date
+        , total_fee_usd / num_txn as avg_txn_fee_usd
+    from (
+        select 
+            block_date
+            , count(hash) as num_txn
+            , sum(txn.gas_used * txn.gas_price /1e18 * p.price) as total_fee_usd
+        from avalanche_c.transactions txn
+        inner join prices_usd p
+            on p.minute = date_trunc('minute',txn.block_time)
+        where txn.block_date between current_date - interval '{{back_days}}' day and current_date - interval '1' day
+            and p.blockchain is null 
+            and p.symbol = 'AVAX'
+        group by 1
+    )
+)
 
 select * from arbitrum_daily_fee
