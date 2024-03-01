c6a6bbb33b4e99b090799c5d32d18971e6933168
stasi <stasi009@163.com>
2024-03-01 16:34:25 +0800

    save

--- Dune/Blockchain Comparison/main_compare_fees.sql
+++ Dune/Blockchain Comparison/main_compare_fees.sql
@@ -12,15 +12,21 @@ with prices_usd as (
 
 , ethereum_daily_fee as (
     select 
-        block_date
-        , count(hash) as num_txn
-        , count(hash) filter (where success) as num_success_txn
-        , count(distinct "from") as num_users
-    from ethereum.transactions txn
-    inner join prices_usd p
-        on p.minute = date_trunc('minute',txn.block_time)
-    where txn.block_date between current_date - interval '{{back_days}}' day and current_date - interval '1' day
-        and p.blockchain is null 
-        and p.symbol = 'ETH'
-    group by 1
+        block_date,
+        total_fee_usd / num_txn as avg_txn_fee_usd
+    from (
+        select 
+            block_date
+            , count(hash) as num_txn
+            , sum(txn.gas_price / 1e18 * txn.gas_used * p.price) as total_fee_usd
+        from ethereum.transactions txn
+        inner join prices_usd p
+            on p.minute = date_trunc('minute',txn.block_time)
+        where txn.block_date between current_date - interval '{{back_days}}' day and current_date - interval '1' day
+            and p.blockchain is null 
+            and p.symbol = 'ETH'
+        group by 1
+    )
 )
+
+select * from ethereum_daily_fee
