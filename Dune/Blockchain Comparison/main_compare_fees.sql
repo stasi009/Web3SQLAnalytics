cd9b8b4f3e7098e3e75a02bc12cf8e7a997fcba9
stasi <stasi009@163.com>
2024-03-01 16:58:47 +0800

    save

--- Dune/Blockchain Comparison/main_compare_fees.sql
+++ Dune/Blockchain Comparison/main_compare_fees.sql
@@ -11,23 +11,24 @@ with prices_usd as (
 )
 
 , ethereum_daily_fee as (
-    select 
-        'ethereum' as blockchain
-        , block_date
-        , total_fee_usd / num_txn as avg_txn_fee_usd
-    from (
+    with txn_fee_usd as (
         select 
             block_date
-            , count(hash) as num_txn
-            , sum(txn.gas_price / 1e18 * txn.gas_used * p.price) as total_fee_usd
+            , txn.gas_price / 1e18 * txn.gas_used * p.price as fee_usd
         from ethereum.transactions txn
         inner join prices_usd p
             on p.minute = date_trunc('minute',txn.block_time)
         where txn.block_date between current_date - interval '{{back_days}}' day and current_date - interval '1' day
             and p.blockchain is null 
             and p.symbol = 'ETH'
-        group by 1
     )
+    select 
+        block_date
+        , 'ethereum' as blockchain
+        , avg(fee_usd) as avg_txnfee_usd
+        , approx_percentile(fee_usd, 0.5) as median_txnfee_usd
+    from txn_fee_usd
+    group by 1
 )
 
 , arbitrum_daily_fee as (
@@ -70,4 +71,5 @@ with prices_usd as (
     )
 )
 
-select * from arbitrum_daily_fee
+select * from ethereum_daily_fee
+
