a48f6eb69591c5ccad4d2071ce5b4336fd16dea5
stasi <stasi009@163.com>
2024-03-04 11:09:29 +0800

    save

--- Dune/AI Narrative Tokens/ai_tokens_total_supply.sql
+++ Dune/AI Narrative Tokens/ai_tokens_total_supply.sql
@@ -1,22 +1,33 @@
 
-with ai_tokens as (
-    select   
-        ait.name
-        , ait.symbol
-        , ait.address as token_address
-        , tki.decimals
-    from query_3486591 ait
-    inner join tokens.erc20 tki
-        on ait.address = tki.contract_address
-        and ait.symbol = tki.symbol
-    where tki.blockchain = 'ethereum'
+with weekly_mint as (
+    select 
+        date_trunc('week',block_time) as week
+        , token_name
+        , token_address
+        , sum(value_adjdec) as mint_val_adjdec
+    from query_3486854 as ait -- ai token transfer
+    where ait."from" = 0x0000000000000000000000000000000000000000
+    group by 1,2,3
+)
+
+, weekly_burn as (
+    select 
+        date_trunc('week',block_time) as week
+        , token_name
+        , token_address
+        , sum(value_adjdec) as burn_val_adjdec
+    from query_3486854 as ait -- ai token transfer
+    where ait.to = 0x0000000000000000000000000000000000000000
+    group by 1,2,3
 )
 
 select 
-    tsf.*
-from ai_tokens ait 
-inner join erc20_ethereum.evt_Transfer tsf
-    on ait.token_address = tsf.contract_address
-where tsf.evt_block_time >= date '2018-04-30'
-    and tsf."from" = 0x0000000000000000000000000000000000000000
-limit 10
+    mint.week 
+    , mint.token_name
+    , mint.token_address
+    , mint.mint_val_adjdec
+    , burn.burn_val_adjdec
+    , (mint.mint_val_adjdec - burn.burn_val_adjdec) as net_supply
+from weekly_mint mint 
+inner join weekly_burn burn 
+    using (week, token_name, token_address)
