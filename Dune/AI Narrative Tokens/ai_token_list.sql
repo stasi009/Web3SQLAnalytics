744794531fa02d5362f36c8894acf7b09a8e00f1
stasi <stasi009@163.com>
2024-03-04 11:26:51 +0800

    save

--- Dune/AI Narrative Tokens/ai_token_list.sql
+++ Dune/AI Narrative Tokens/ai_token_list.sql
@@ -100,5 +100,4 @@ where tki.blockchain = 'ethereum'
             ,'WLD'
             ,'AGIX'
             ,'RNDR'
-            ,'VRA'
         )
--- Dune/AI Narrative Tokens/ai_tokens_total_supply.sql
+++ Dune/AI Narrative Tokens/ai_tokens_total_supply.sql
@@ -1,33 +1,37 @@
 
-with weekly_mint as (
+with mint as (
     select 
-        date_trunc('week',block_time) as week
+        block_time
         , token_name
         , token_address
-        , sum(value_adjdec) as mint_val_adjdec
+        , value_adjdec as supply
     from query_3486854 as ait -- ai token transfer
     where ait."from" = 0x0000000000000000000000000000000000000000
-    group by 1,2,3
 )
 
-, weekly_burn as (
+, burn as (
     select 
-        date_trunc('week',block_time) as week
+        block_time
         , token_name
         , token_address
-        , sum(value_adjdec) as burn_val_adjdec
+        , -1*value_adjdec as supply
     from query_3486854 as ait -- ai token transfer
     where ait.to = 0x0000000000000000000000000000000000000000
-    group by 1,2,3
+)
+
+, week_last_price as (
+
 )
 
 select 
-    mint.week 
-    , mint.token_name
-    , mint.token_address
-    , mint.mint_val_adjdec
-    , burn.burn_val_adjdec
-    , (mint.mint_val_adjdec - burn.burn_val_adjdec) as net_supply
-from weekly_mint mint 
-inner join weekly_burn burn 
-    using (week, token_name, token_address)
+    date_trunc('week',block_time) as week
+    , token_name
+    , token_address
+    , sum(supply) as week_net_supply
+from (
+    select * from mint 
+    union all 
+    select * from burn
+)
+group by 1,2,3
+order by 1,2,3
