201567e6b8bf2917310fae1691a41b1553a92a55
stasi <stasi009@163.com>
2024-03-05 09:07:47 +0800

    save

--- Dune/AI Narrative Tokens/one_ai_token_trade.sql
+++ Dune/AI Narrative Tokens/one_ai_token_trade.sql
@@ -26,44 +26,68 @@ with one_ai_token as (
     where td.blockchain = 'ethereum'
 )
 
+, weekly_volume as (
+    select 
+        week 
+
+        , sum(td.amount_usd) as total_volume
+        , sum(td.amount_usd) filter (where trade_direction = 'buy') as buy_volume
+        , sum(td.amount_usd) filter (where trade_direction = 'sell') as sell_volume
+        
+        , approx_distinct(td.trader) as total_traders
+        , approx_distinct(td.trader) filter (where trade_direction = 'buy') as total_buyers
+        , approx_distinct(td.trader) filter (where trade_direction = 'sell') as total_sellers
+    from trades td
+    group by 1
+)
+
 , valid_prices as (
     select 
         p.minute
         , date_trunc('week',p.minute) as week
         , ait.token_address
-        , ait.name as token_name
+        , ait.token_name as token_name
         , p.price 
     from prices.usd p
     inner join one_ai_token ait-- ai token list
         on p.contract_address = ait.token_address
-        and p.symbol = ait.symbol
+        and p.symbol = ait.token_symbol
         and p.minute >= ait.launch_date
     and p.blockchain = 'ethereum'
 )
 
-select 
-    week 
-    , token_address
-    , token_name
-    , price
-from (
+, weekly_last_price as (
     select 
-        week
-        , minute
+        week 
         , token_address
         , token_name
         , price
-        , row_number() over (partition by week,token_address order by minute desc) as rn
-    from valid_prices
+    from (
+        select 
+            week
+            , minute
+            , token_address
+            , token_name
+            , price
+            , row_number() over (partition by week,token_address order by minute desc) as rn
+        from valid_prices
+    )
+    where rn = 1
 )
-where rn = 1
-order by 1
 
 select 
     week 
-    , sum(amount_usd) as total_volume
-    , sum(amount_usd) filter (where trade_direction = 'buy') as buy_volume
-    , -1 * sum(amount_usd) filter (where trade_direction = 'sell') as sell_volume
-    , approx_distinct(trader) as traders
-from trades
-group by 1
+
+    , vol.total_volume
+    , vol.buy_volume
+    , -1 * vol.sell_volume as sell_volume
+
+    , vol.total_traders
+    , vol.total_buyers
+    , -1 * vol.total_sellers as total_sellers 
+
+    , wp.price
+from weekly_volume as vol
+inner join weekly_last_price wp
+    using (week)
+order by 1
