494fd194c6d14e4328e3e485637afb854f14838a
stasi <stasi009@163.com>
2024-03-15 14:34:05 +0800

    save

--- Dune/BlastActionOnL1/mq_daily_deposit.sql
+++ Dune/BlastActionOnL1/mq_daily_deposit.sql
@@ -1,8 +1,10 @@
 
 with Event_ETHBridgeInitiated as (
     select 
-        varbinary_ltrim(topic1) as from_address
-        , bytearray_ltrim(topic2) as to_address -- bytearray_xxx与varbinary_xxx互为alias
+        block_date
+        , tx_hash 
+        , 'ETH' as token
+        , varbinary_ltrim(topic1) as sender
         , varbinary_to_uint256(varbinary_substring(data,1,32)) as amount
     from ethereum.logs
     where contract_address = 0x3a05E5d33d7Ab3864D53aaEc93c8301C1Fa49115 -- Blast: L1 Bridge Proxy
@@ -12,27 +14,29 @@ with Event_ETHBridgeInitiated as (
 
 , Event_ERC20BridgeInitiated_stETH as (
     select 
-        varbinary_ltrim(topic1) as localToken
-        , varbinary_ltrim(topic2) as remoteToken
-        , varbinary_ltrim(topic3) as from_address
-        , varbinary_ltrim(varbinary_substring(data,1,32)) as to_address 
+        block_date 
+        , tx_hash
+        , 'stETH' as token
+        , varbinary_ltrim(topic3) as sender
         , varbinary_to_uint256(varbinary_substring(data,1+32,32)) as amount
     from ethereum.logs
     where contract_address = 0x3a05E5d33d7Ab3864D53aaEc93c8301C1Fa49115 -- Blast: L1 Bridge Proxy
         and topic0 = 0x7ff126db8024424bbfd9826e8ab82ff59136289ea440b04b39a0df1b03b9cabf -- ERC20BridgeInitiated
+        and varbinary_ltrim(topic1) = 0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84 -- local token is stETH
         and varbinary_ltrim(topic2) = 0x -- 说明topic2全0，表明在L2 chain上存储的是native ETH
         and block_date between current_date - interval '{{back_days}}' day and current_date - interval '1' day
 )
 
 , Event_ERC20BridgeInitiated_StableCoin as (
     select 
-        varbinary_ltrim(topic1) as localToken
-        , varbinary_ltrim(topic2) as remoteToken
-        , varbinary_ltrim(topic3) as from_address
-        , varbinary_ltrim(varbinary_substring(data,1,32)) as to_address 
+        block_date
+        , tx_hash
+        , varbinary_ltrim(topic1) as localToken
+        , varbinary_ltrim(topic3) as sender
         , varbinary_to_uint256(varbinary_substring(data,1+32,32)) as amount
     from ethereum.logs
     where contract_address = 0x3a05E5d33d7Ab3864D53aaEc93c8301C1Fa49115 -- Blast: L1 Bridge Proxy
         and topic0 = 0x7ff126db8024424bbfd9826e8ab82ff59136289ea440b04b39a0df1b03b9cabf -- ERC20BridgeInitiated
+        and varbinary_ltrim(topic2) = 0x4300000000000000000000000000000000000003 -- USDB on blast L2
         and block_date between current_date - interval '{{back_days}}' day and current_date - interval '1' day
 )
