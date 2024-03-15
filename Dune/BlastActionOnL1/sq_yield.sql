015f1f331883b9b4b0f6d00cc354186bd0841c1e
stasi <stasi009@163.com>
2024-03-15 19:56:20 +0800

    save

--- Dune/BlastActionOnL1/sq_yield.sql
+++ Dune/BlastActionOnL1/sq_yield.sql
@@ -1,6 +1,19 @@
 
 select 
-    varbinary_to_int256(varbinary_substring(data,1,32))/1e18 as yield -- not uint256, because yield can be negative
+    block_date,
+    , tx_hash
+
+    , case 
+        when contract_address = 0x98078db053902644191f93988341E31289E1C8FE then 'ETH'
+        when contract_address = 0xa230285d5683C74935aD14c446e137c8c8828438 then 'USD'
+    end as yield_concurrency
+
+    , case 
+        when contract_address = 0x98078db053902644191f93988341E31289E1C8FE then 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2 -- use WETH to query price
+        when contract_address = 0xa230285d5683C74935aD14c446e137c8c8828438 then 0x6B175474E89094C44Da98b954EedeAC495271d0F -- use Dai to query price
+    end as price_token
+
+    , varbinary_to_int256(varbinary_substring(data,1,32))/1e18 as yield -- not uint256, because yield can be negative
     , varbinary_to_uint256(varbinary_substring(data,1+32,32))/1e18 as insurancePremiumPaid
     , varbinary_to_uint256(varbinary_substring(data,1+2*32,32))/1e18 as insuranceWithdrawn
 from ethereum.logs
