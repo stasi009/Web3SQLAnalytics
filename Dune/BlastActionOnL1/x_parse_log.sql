7f36f7c7d897e040bf8a43188fdbd8f2023d0fd3
stasi <stasi009@163.com>
2024-03-15 11:30:23 +0800

    save

--- Dune/BlastActionOnL1/x_parse_log.sql
+++ Dune/BlastActionOnL1/x_parse_log.sql
@@ -74,4 +74,17 @@ with Event_ETHBridgeInitiated as (
         and tx_hash = 0xc71754b8c69becaf1cd5b35ecc07b4213c2b2a558b594f4f486c4ad8e991671e
 )
 
-select * from Event_LidoYieldProvider_Claimed
+, Event_ETHYieldManager_WithdrawRequested as (
+    select 
+        varbinary_to_uint256(topic1) as requestId
+        , bytearray_ltrim(topic2) as requestor 
+        , bytearray_ltrim(topic3) as recipient 
+        , varbinary_to_uint256(varbinary_substring(data,1,32)) as amount
+    from ethereum.logs
+    where contract_address = 0x98078db053902644191f93988341E31289E1C8FE -- Blast: ETH Yield Manager Proxy
+        and topic0 = 0x00ae2c76ca218353c7995e13a4af773a35837cb6ebb8288092d8190bcd9c8f68 -- WithdrawalRequested
+        and block_date = date '2024-03-01' 
+        and tx_hash = 0x1884c39b7a9c598d92bf0ecd2e2636a33cd44dfb4186476901eeedceea5e9754
+)
+
+select * from Event_ETHYieldManager_WithdrawRequested
