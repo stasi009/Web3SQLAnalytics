eba3ea4a407aba9f38908d61ab008d661818b70d
stasi <stasi009@163.com>
2024-03-15 10:35:04 +0800

    save

--- Dune/BlastActionOnL1/x_parse_log.sql
+++ Dune/BlastActionOnL1/x_parse_log.sql
@@ -23,6 +23,7 @@ with Event_ETHBridgeInitiated as (
         and topic0 = 0x7ff126db8024424bbfd9826e8ab82ff59136289ea440b04b39a0df1b03b9cabf -- ERC20BridgeInitiated
         and block_date = date '2024-03-11' 
         and tx_hash = 0xa09595ea792df62cd28749a97b7a53bb5ce7ed2e82e4b66fcab006278d81b6a9
+        and varbinary_ltrim(topic2) = 0x -- 说明topic2全0，表明在L2 chain上存储的是native ETH
 )
 
 , Event_LidoStaked as (
@@ -36,4 +37,18 @@ with Event_ETHBridgeInitiated as (
         and tx_hash = 0xa09595ea792df62cd28749a97b7a53bb5ce7ed2e82e4b66fcab006278d81b6a9
 )
 
-select * from Event_LidoStaked
+, Event_ERC20BridgeInitiated_StableCoin as (
+    select 
+        varbinary_ltrim(topic1) as localToken
+        , varbinary_ltrim(topic2) as remoteToken
+        , varbinary_ltrim(topic3) as from_address
+        , varbinary_ltrim(varbinary_substring(data,1,32)) as to_address 
+        , varbinary_to_uint256(varbinary_substring(data,1+32,32)) as amount
+    from ethereum.logs
+    where contract_address = 0x3a05E5d33d7Ab3864D53aaEc93c8301C1Fa49115 -- Blast: L1 Bridge Proxy
+        and topic0 = 0x7ff126db8024424bbfd9826e8ab82ff59136289ea440b04b39a0df1b03b9cabf -- ERC20BridgeInitiated
+        and block_date = date '2024-03-11' 
+        and tx_hash = 0xf94091a6c70989cf387e391a436be70f1ce4035fcddf4b887edf8ddf5cc00832
+)
+
+select * from Event_ERC20BridgeInitiated_StableCoin
