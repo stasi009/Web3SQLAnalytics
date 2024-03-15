b203d084f22335a410cfd2c637a3a5892d6ac17a
stasi <stasi009@163.com>
2024-03-15 17:18:04 +0800

    save

---
+++ Dune/BlastActionOnL1/mq_daily_new_depositors.sql
@@ -0,0 +1,29 @@
+
+with first_deposit_day as (
+    select 
+        sender 
+        , min(block_date) as first_deposit_day
+    from query_3527756 -- sq_all_deposits
+    group by 1 -- 只根据daily来group，不涉及币种
+)
+
+, new_or_old_deposits as (
+    select 
+        de.block_date
+        , de.sender 
+        , case 
+            when de.block_date = day1.first_deposit_day then 'new user'
+            else 'old user'
+        end as sender_new_old
+    from query_3527756 de -- sq_all_deposits
+    inner join first_deposit_day day1
+        on de.sender = day1.sender
+    where block_date between current_date - interval '{{back_days}}' day and current_date - interval '1' day
+)
+
+select 
+    block_date 
+    , sender_new_old
+    , approx_distinct(sender) as num_depositors
+from new_or_old_deposits
+group by 1,2
