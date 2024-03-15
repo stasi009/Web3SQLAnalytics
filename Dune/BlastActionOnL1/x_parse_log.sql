4795fd964cd7f558a33b2fbf11d2a8ae86a273a1
stasi <stasi009@163.com>
2024-03-15 11:47:00 +0800

    save

--- Dune/BlastActionOnL1/x_parse_log.sql
+++ Dune/BlastActionOnL1/x_parse_log.sql
@@ -87,4 +87,40 @@ with Event_ETHBridgeInitiated as (
         and tx_hash = 0x1884c39b7a9c598d92bf0ecd2e2636a33cd44dfb4186476901eeedceea5e9754
 )
 
-select * from Event_ETHYieldManager_WithdrawRequested
+, Event_ETHYieldManager_WithdrawClaimed as (
+    select 
+        varbinary_to_uint256(topic1) as requestId
+        , bytearray_ltrim(topic2) as recipient 
+        , varbinary_to_uint256(varbinary_substring(data,1,32)) as amount
+    from ethereum.logs
+    where contract_address = 0x98078db053902644191f93988341E31289E1C8FE -- Blast: ETH Yield Manager Proxy
+        and topic0 = 0x8adb7a84b2998a8d11cd9284395f95d5a99f160be785ae79998c654979bd3d9a -- WithdrawalClaimed
+        and block_date = date '2024-03-13' 
+        and tx_hash = 0xbf125c91ce24cf9f98bd2302550f54b5c28590a98df870383910e8c59a39752f
+)
+
+, Event_ETHYieldManager_YieldReport as (
+    select 
+        varbinary_to_int256(varbinary_substring(data,1,32)) as yield -- not uint256, because yield can be negative
+        , varbinary_to_uint256(varbinary_substring(data,1+32,32)) as insurancePremiumPaid
+        , varbinary_to_uint256(varbinary_substring(data,1+2*32,32)) as insuranceWithdrawn
+    from ethereum.logs
+    where contract_address = 0x98078db053902644191f93988341E31289E1C8FE -- Blast: ETH Yield Manager Proxy
+        and topic0 = 0x00de4b58e7863b1e3dce7259a138136239427388d53e4844f369cdee7a81dbf5 -- YieldReport
+        and block_date = date '2024-03-11' 
+        and tx_hash = 0xd55ff8f9eaf4867d126bfec77e5d7d15200d22565259f0b7e013b897ca02e92b
+)
+
+, Event_USDYieldManager_YieldReport as (
+    select 
+        varbinary_to_int256(varbinary_substring(data,1,32)) as yield -- not uint256, because yield can be negative
+        , varbinary_to_uint256(varbinary_substring(data,1+32,32)) as insurancePremiumPaid
+        , varbinary_to_uint256(varbinary_substring(data,1+2*32,32)) as insuranceWithdrawn
+    from ethereum.logs
+    where contract_address = 0xa230285d5683C74935aD14c446e137c8c8828438 -- Blast: USD Yield Manager Proxy
+        and topic0 = 0x00de4b58e7863b1e3dce7259a138136239427388d53e4844f369cdee7a81dbf5 -- YieldReport
+        and block_date = date '2024-03-13' 
+        and tx_hash = 0x55766ee1cf72625691d694fdc32758efe75a2f1e1959e6d3c88d8554d794056f
+)
+
+select * from Event_USDYieldManager_YieldReport
