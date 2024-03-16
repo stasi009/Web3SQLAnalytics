2ce1bdc55ed1e34d0535639975ca1749e26edca9
stasi <stasi009@163.com>
2024-03-16 16:41:41 +0800

    save

--- Flipside/DencunImpactOnL2/m_arbitrum_L1fee_dencun.sql
+++ Flipside/DencunImpactOnL2/m_arbitrum_L1fee_dencun.sql
@@ -6,51 +6,25 @@ with arbitrum_fee_on_L1 as (
         , count(txns.tx_hash) as num_txn
 
         , avg(gas_used) as avg_gas_used
-        , avg(tx_fee) as avg_tx_fee
+        , avg(tx_fee) as avg_tx_fee_eth
 
+        , APPROX_PERCENTILE(gas_used,0.5) as median_gas_used
+        , APPROX_PERCENTILE(tx_fee,0.5) as median_tx_fee_eth
+
+        , avg(gas_price) as avg_gas_price_gwei
     from ethereum.core.fact_transactions txns
     where txns.to_address = lower('0x1c479675ad559DC151F6Ec7ed3FbF8ceE79582B6') -- 'Arbitrum: Sequencer Inbox'
+        and txns.block_timestamp::date >= '2024-03-12'
+        and txns.block_timestamp < date_trunc('hour',current_timestamp) -- reduce impact of incomplete hour
     group by 1
 )
 
-, fee_before_dencun as (
-    select 
-        hour
 
-        , num_txn as pre_dencun_num_txn
-        , avg_gas_used as pre_dencun_avg_gas_used
-        , avg_tx_fee as pre_dencun_avg_txfee
-
-        , null as post_dencun_num_txn
-        , null as post_dencun_avg_gas_used
-        , null as post_dencun_avg_txfee
-
-    from arbitrum_fee_on_L1
-    where hour::date >= '2024-03-12'
-        and hour <= timestamp '2024-03-13 13:55:59'
-    order by 1
-)
-
-, fee_after_dencun as (
-    select 
-        hour
-
-        , null as pre_dencun_num_txn
-        , null as pre_dencun_avg_gas_used
-        , null as pre_dencun_avg_txfee
-
-        , num_txn as post_dencun_num_txn
-        , avg_gas_used as post_dencun_avg_gas_used
-        , avg_tx_fee as post_dencun_avg_txfee
-
-    from arbitrum_fee_on_L1
-    where hour > timestamp '2024-03-13 13:55:59'
-        and hour < date_trunc('hour',current_timestamp) -- reduce impact of incomplete hour
-    order by 1
-)
-
-select * from fee_before_dencun
-union all
-select * from fee_after_dencun
-
-
+select 
+    *
+    , case 
+        when hour <= timestamp '2024-03-13 13:55:59' then 'Before Dencun'
+        else 'After Dencun'
+    end as dencun_flag
+from arbitrum_fee_on_L1
+order by hour
