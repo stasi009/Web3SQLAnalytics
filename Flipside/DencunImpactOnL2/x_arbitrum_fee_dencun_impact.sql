0f43ffcdeff09243d430d678f08173190c015988
stasi <stasi009@163.com>
2024-03-16 14:51:39 +0800

    save

--- Flipside/DencunImpactOnL2/x_arbitrum_fee_dencun_impact.sql
+++ Flipside/DencunImpactOnL2/x_arbitrum_fee_dencun_impact.sql
@@ -3,9 +3,9 @@ with ArbitrumProxyOnL1 as (
     values
 
         ('0x4c6f947Ae67F572afa4ae0730947DE7C874F95Ef', 'Arbitrum: Sequencer Inbox')
-        , ('0x51de512aa5dfb02143a91c6f772261623ae64564', 'Arbitrum: Validator1')
+        -- , ('0x51de512aa5dfb02143a91c6f772261623ae64564', 'Arbitrum: Validator1')
         , ('0x1c479675ad559DC151F6Ec7ed3FbF8ceE79582B6', 'Arbitrum: Sequencer Inbox NITRO')
-        , ('0x72Ce9c846789fdB6fC1f34aC4AD25Dd9ef7031ef', 'Arbitrum: Gateway Router')
+        -- , ('0x72Ce9c846789fdB6fC1f34aC4AD25Dd9ef7031ef', 'Arbitrum: Gateway Router')
 
     as my_table(address, name)
 )
