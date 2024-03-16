1464141537a43eade6a344371df51ec6dd44b8e7
stasi <stasi009@163.com>
2024-03-16 16:03:24 +0800

    save

--- Dune/misc/L2proxyOnL1_dune.sql
+++ Dune/misc/L2proxyOnL1_dune.sql
@@ -2,9 +2,7 @@
 with L2proxyOnL1(address, name) as (
     values
 
-    (0x4c6f947Ae67F572afa4ae0730947DE7C874F95Ef, 'Arbitrum: Sequencer Inbox'), 
-    (0x51de512aa5dfb02143a91c6f772261623ae64564, 'Arbitrum: Validator1'),
-    (0x1c479675ad559DC151F6Ec7ed3FbF8ceE79582B6, 'Arbitrum: Sequencer Inbox NITRO'),
+    (0x1c479675ad559DC151F6Ec7ed3FbF8ceE79582B6, 'Arbitrum: Sequencer Inbox'),
     (0x72Ce9c846789fdB6fC1f34aC4AD25Dd9ef7031ef, 'Arbitrum: Gateway Router'),
 
     (0x211E1c4c7f1bF5351Ac850Ed10FD68CFfCF6c21b, 'Arbitrum Nova: Sequencer Inbox'), 
