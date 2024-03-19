75aa6c06094e17f682c8e7d8bc0a3740445aa732
stasi <stasi009@163.com>
2024-03-19 08:56:59 +0800

    save

--- Dune/BlastActionOnL1/x_parse_log.sql
+++ Dune/BlastActionOnL1/x_parse_log.sql
@@ -178,5 +178,22 @@ with Event_ETHBridgeInitiated as (
     where requestId=0
 )
 
-select * from Event_OptimismPortalProxy_Withdraw_StableCoin
+, Event_DsrYieldManager_WithdrawRequested as (
+    select 
+        block_date
+        , tx_hash
+        , tx_from as user
+        -- 不能用recipient,它们不是真正的收款人，而是Blast: Optimism Portal Proxy
+        -- , bytearray_ltrim(topic2) as requestor 
+        -- , bytearray_ltrim(topic3) as recipient 
+        , varbinary_to_uint256(varbinary_substring(data,1,32)) as amount
+    from ethereum.logs
+    where contract_address = 0xa230285d5683C74935aD14c446e137c8c8828438 -- Blast: USD Yield Manager Proxy
+        and topic0 = 0x00ae2c76ca218353c7995e13a4af773a35837cb6ebb8288092d8190bcd9c8f68 -- WithdrawalRequested
+        and block_date >= date '2024-02-24' -- day when blast L1 bridge is deployed
+)
+
+
+
+select * from Event_DsrYieldManager_WithdrawRequested
 order by block_time desc
