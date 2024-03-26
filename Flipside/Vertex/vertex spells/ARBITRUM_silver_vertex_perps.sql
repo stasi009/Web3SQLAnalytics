27ec983814d5be5b1864d52f7389a0a34f89f028
stasi <stasi009@163.com>
2024-03-26 08:24:12 +0800

    save

--- Flipside/Vertex/vertex spells/ARBITRUM_silver_vertex_perps.sql
+++ Flipside/Vertex/vertex spells/ARBITRUM_silver_vertex_perps.sql
@@ -69,40 +69,16 @@ order_fill_decode AS (
         regexp_substr_all(SUBSTR(DATA, 3, len(DATA)), '.{64}') AS segmented_data,
         topics [1] :: STRING AS digest,
         --unique hash of the order
-        LEFT(
-            topics [2] :: STRING,
-            42
-        ) AS trader,
+        LEFT(  topics [2] :: STRING,   42 ) AS trader,
         topics [2] :: STRING AS subaccount,
-        utils.udf_hex_to_int(
-            segmented_data [0] :: STRING
-        ) :: INT AS pricex18,
-        utils.udf_hex_to_int(
-            's2c',
-            segmented_data [1] :: STRING
-        ) :: INT AS amount,
-        utils.udf_hex_to_int(
-            segmented_data [2] :: STRING
-        ) AS expiration,
-        utils.udf_hex_to_int(
-            segmented_data [3] :: STRING
-        ) :: INT AS nonce,
-        utils.udf_hex_to_int(
-            's2c',
-            segmented_data [4] :: STRING
-        ) :: INT AS isTaker,
-        utils.udf_hex_to_int(
-            's2c',
-            segmented_data [5] :: STRING
-        ) :: INT AS feeAmount,
-        utils.udf_hex_to_int(
-            's2c',
-            segmented_data [6] :: STRING
-        ) :: INT AS baseDelta,
-        utils.udf_hex_to_int(
-            's2c',
-            segmented_data [7] :: STRING
-        ) :: INT AS quoteDelta,
+        utils.udf_hex_to_int(  segmented_data [0] :: STRING  ) :: INT AS pricex18,
+        utils.udf_hex_to_int( 's2c',   segmented_data [1] :: STRING ) :: INT AS amount,
+        utils.udf_hex_to_int( segmented_data [2] :: STRING ) AS expiration,
+        utils.udf_hex_to_int( segmented_data [3] :: STRING ) :: INT AS nonce,
+        utils.udf_hex_to_int( 's2c',  segmented_data [4] :: STRING ) :: INT AS isTaker,
+        utils.udf_hex_to_int( 's2c', segmented_data [5] :: STRING ) :: INT AS feeAmount,
+        utils.udf_hex_to_int( 's2c',  segmented_data [6] :: STRING ) :: INT AS baseDelta,
+        utils.udf_hex_to_int( 's2c',  segmented_data [7] :: STRING ) :: INT AS quoteDelta,
         l._log_id,
         l._inserted_timestamp
     FROM
@@ -124,54 +100,26 @@ order_fill_decode_v2 AS (
         origin_from_address,
         origin_to_address,
         regexp_substr_all(SUBSTR(DATA, 3, len(DATA)), '.{64}') AS segmented_data,
-        utils.udf_hex_to_int(
-            topics [1] :: STRING
-        ) :: STRING AS product_id,
+        utils.udf_hex_to_int( topics [1] :: STRING ) :: STRING AS product_id,
         s.symbol,
         topics [2] :: STRING AS digest,
         --unique hash of the order
-        LEFT(
-            topics [3] :: STRING,
-            42
-        ) AS trader,
+        LEFT( topics [3] :: STRING,   42 ) AS trader,
         topics [3] :: STRING AS subaccount,
-        utils.udf_hex_to_int(
-            segmented_data [0] :: STRING
-        ) :: INT AS pricex18,
-        utils.udf_hex_to_int(
-            's2c',
-            segmented_data [1] :: STRING
-        ) :: INT AS amount,
-        utils.udf_hex_to_int(
-            segmented_data [2] :: STRING
-        ) AS expiration,
-        utils.udf_hex_to_int(
-            segmented_data [3] :: STRING
-        ) :: INT AS nonce,
-        utils.udf_hex_to_int(
-            's2c',
-            segmented_data [4] :: STRING
-        ) :: INT AS isTaker,
-        utils.udf_hex_to_int(
-            's2c',
-            segmented_data [5] :: STRING
-        ) :: INT AS feeAmount,
-        utils.udf_hex_to_int(
-            's2c',
-            segmented_data [6] :: STRING
-        ) :: INT AS baseDelta,
-        utils.udf_hex_to_int(
-            's2c',
-            segmented_data [7] :: STRING
-        ) :: INT AS quoteDelta,
+        utils.udf_hex_to_int(  segmented_data [0] :: STRING ) :: INT AS pricex18,
+        utils.udf_hex_to_int( 's2c',   segmented_data [1] :: STRING ) :: INT AS amount,
+        utils.udf_hex_to_int( segmented_data [2] :: STRING) AS expiration,
+        utils.udf_hex_to_int(  segmented_data [3] :: STRING) :: INT AS nonce,
+        utils.udf_hex_to_int('s2c', segmented_data [4] :: STRING) :: INT AS isTaker,
+        utils.udf_hex_to_int('s2c',  segmented_data [5] :: STRING) :: INT AS feeAmount,
+        utils.udf_hex_to_int('s2c',segmented_data [6] :: STRING ) :: INT AS baseDelta,
+        utils.udf_hex_to_int('s2c',segmented_data [7] :: STRING) :: INT AS quoteDelta,
         l._log_id,
         l._inserted_timestamp
     FROM
         logs l
         INNER JOIN vertex_products s
-        ON s.product_id = utils.udf_hex_to_int(
-            topics [1] :: STRING
-        )
+        ON s.product_id = utils.udf_hex_to_int( topics [1] :: STRING )
     WHERE
         topics [0] :: STRING = '0x7c57459d6f4f0fb2fc5b1e298c8c0eb238422944964aa1e249eaa78747f0cca9'
         AND contract_address = LOWER('0xa4369d8e3dc847aedf17f4125f1abb1bc18fc060')
@@ -199,42 +147,27 @@ order_fill_format AS (
         utils.udf_binary_to_int(SUBSTR(exp_binary, -3, 1)) AS market_reduce_flag,
         CASE
             WHEN len(expiration) < 11 THEN TRY_TO_TIMESTAMP(utils.udf_binary_to_int(exp_binary) :: STRING)
-            ELSE TRY_TO_TIMESTAMP(
-                utils.udf_binary_to_int(SUBSTR(exp_binary, 24)) :: STRING
-            )
+            ELSE TRY_TO_TIMESTAMP( utils.udf_binary_to_int(SUBSTR(exp_binary, 24)) :: STRING )
         END AS expiration,
         nonce,
         isTaker,
         feeAmount AS fee_amount_unadj,
-        feeAmount / pow(
-            10,
-            18
-        ) AS fee_amount,
+        feeAmount / pow(  10,  18 ) AS fee_amount,
         pricex18 AS price_amount_unadj,
-        pricex18 / pow(
-            10,
-            18
-        ) AS price_amount,
+        pricex18 / pow( 10, 18 ) AS price_amount,
         amount AS amount_unadj,
-        amount / pow(
-            10,
-            18
-        ) AS amount,
+        amount / pow(10,  18 ) AS amount,
         baseDelta AS base_delta_unadj,
-        baseDelta / pow(
-            10,
-            18
-        ) AS base_delta,
+        baseDelta / pow( 10, 18) AS base_delta,
         quoteDelta AS quote_delta_unadj,
-        quoteDelta / pow(
-            10,
-            18
-        ) AS quote_delta,
+        quoteDelta / pow( 10,  18 ) AS quote_delta,
         _log_id,
         _inserted_timestamp
     FROM
         order_fill_decode
+    
     UNION ALL
+
     SELECT
         block_number,
         block_timestamp,
@@ -257,37 +190,20 @@ order_fill_format AS (
         utils.udf_binary_to_int(SUBSTR(exp_binary, -3, 1)) AS market_reduce_flag,
         CASE
             WHEN len(expiration) < 11 THEN TRY_TO_TIMESTAMP(utils.udf_binary_to_int(exp_binary) :: STRING)
-            ELSE TRY_TO_TIMESTAMP(
-                utils.udf_binary_to_int(SUBSTR(exp_binary, 24)) :: STRING
-            )
+            ELSE TRY_TO_TIMESTAMP(  utils.udf_binary_to_int(SUBSTR(exp_binary, 24)) :: STRING)
         END AS expiration,
         nonce,
         isTaker,
         feeAmount AS fee_amount_unadj,
-        feeAmount / pow(
-            10,
-            18
-        ) AS fee_amount,
+        feeAmount / pow(  10,  18 ) AS fee_amount,
         pricex18 AS price_amount_unadj,
-        pricex18 / pow(
-            10,
-            18
-        ) AS price_amount,
+        pricex18 / pow(  10,  18 ) AS price_amount,
         amount AS amount_unadj,
-        amount / pow(
-            10,
-            18
-        ) AS amount,
+        amount / pow(  10,  18 ) AS amount,
         baseDelta AS base_delta_unadj,
-        baseDelta / pow(
-            10,
-            18
-        ) AS base_delta,
+        baseDelta / pow( 10,18 ) AS base_delta,
         quoteDelta AS quote_delta_unadj,
-        quoteDelta / pow(
-            10,
-            18
-        ) AS quote_delta,
+        quoteDelta / pow( 10, 18 ) AS quote_delta,
         _log_id,
         _inserted_timestamp
     FROM
