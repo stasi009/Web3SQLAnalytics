4146cfa6b3db65c677b635aad7702c9a77395a88
stasi <stasi009@163.com>
2024-03-28 14:32:25 +0800

    save

--- Flipside/Vertex/weekly_positions_eth.sql
+++ Flipside/Vertex/weekly_positions_eth.sql
@@ -7,7 +7,7 @@ with weekly_taker_perp_trades as (
         end as action
         , sum(amount_usd) as amount_usd
     from ARBITRUM.vertex.ez_perp_trades
-    where block_timestamp < date_trunc('week',current_date) -- avoid incomplete day
+    where block_timestamp < date_trunc('week',current_date) -- avoid incomplete week
         and is_taker 
         and symbol = 'ETH-PERP'
     group by 1,2
@@ -28,7 +28,7 @@ with weekly_taker_perp_trades as (
         , avg(price) as price
     from ARBITRUM.price.ez_hourly_token_prices
     where hour >= date '2023-03-05' -- vertex go online
-        and hour < date_trunc('week',current_date) -- avoid incomplete day
+        and hour < date_trunc('week',current_date) -- avoid incomplete week
         and token_address = lower('0x82aF49447D8a07e3bd95BD0d56f35241523fBab1') -- WETH
     group by 1
 )
