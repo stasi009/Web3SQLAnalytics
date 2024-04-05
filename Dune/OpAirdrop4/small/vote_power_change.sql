dda05dae4810f79f825dfa982fc448b462aae802
stasi <stasi009@163.com>
2024-04-05 16:26:30 +0800

    save

---
+++ Dune/OpAirdrop4/small/vote_power_change.sql
@@ -0,0 +1,25 @@
+with airdrop as (
+    select 
+        date_trunc('day',min(block_time)) as start_claim_day
+    from optimism.logs
+    where block_date >= date '2024-02-16' -- day when airdrop contract is deployed
+        and contract_address = 0xFb4D5A94b516DF77Fbdbcf3CfeB262baAF7D4dB7 -- airdrop contract
+        and topic0 = 0x4ec90e965519d92681267467f775ada5bd214aa92c0dc93d90a5e880ce9ed026 -- claimed
+)
+
+, daily_last_stat as (
+    select 
+        day
+        , total_voting_power
+        , total_delegators
+    from (
+        select 
+            date_trunc('day',block_time) as day
+            , total_voting_power
+            , total_delegators
+            , row_number() over (partition by date_trunc('day',block_time) order by block_time desc) as rev_rank
+        from op_governance_optimism.delegates
+    )
+    where rev_rank = 1
+)
+
