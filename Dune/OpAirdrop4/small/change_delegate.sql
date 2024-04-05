85660333cac1cdf4dc4f3a7c1faa343e2bb62b8a
stasi <stasi009@163.com>
2024-04-05 09:41:14 +0800

    save

--- Dune/OpAirdrop4/small/change_delegate.sql
+++ Dune/OpAirdrop4/small/change_delegate.sql
@@ -7,7 +7,29 @@ with start_claim as (
         and topic0 = 0x4ec90e965519d92681267467f775ada5bd214aa92c0dc93d90a5e880ce9ed026 -- claimed
 )
 
-, Delegate_Before_Airdrop as (
+, airdrop_claimed as (
+    select 
+        -- tx_hash 
+        -- , block_time
+        varbinary_ltrim(varbinary_substring(data,1+32,32)) as claimer 
+        -- , varbinary_to_uint256(varbinary_substring(data,1+2*32,32)) / 1e18 as op_amt_adjdec
+    from optimism.logs
+    where block_date >= date '2024-02-16' -- day when airdrop contract is deployed
+        and contract_address = 0xFb4D5A94b516DF77Fbdbcf3CfeB262baAF7D4dB7 -- airdrop contract
+        and topic0 = 0x4ec90e965519d92681267467f775ada5bd214aa92c0dc93d90a5e880ce9ed026 -- claimed
+)
+
+, delegate_change_for_claimers as (
+    select
+        del.evt_block_time
+        , del.delegator 
+        , del.toDelegate 
+    from op_optimism.GovernanceToken_evt_DelegateChanged del
+    inner join airdrop_claimed ac  
+        on del.delegator = ac.claimer -- 只关心claimer的delegate change情况(包括airdrop前与后)
+)
+
+, claimer_delegate_before_airdrop as (
     select 
         delegator
         , toDelegate as delegate_before_airdrop
@@ -16,14 +38,14 @@ with start_claim as (
             del.delegator 
             , del.toDelegate 
             , rank() over (partition by del.delegator order by del.evt_block_time desc) as rank
-        from op_optimism.GovernanceToken_evt_DelegateChanged del
+        from delegate_change_for_claimers del
         cross join start_claim sc
         where del.evt_block_time < sc.start_claim_tm
     )
     where rank = 1
 )
 
-, Delegate_After_Airdrop as (
+, claimer_delegate_after_airdrop as (
     select 
         delegator
         , toDelegate as delegate_after_airdrop
@@ -32,25 +54,21 @@ with start_claim as (
             del.delegator 
             , del.toDelegate 
             , rank() over (partition by del.delegator order by del.evt_block_time desc) as rank
-        from op_optimism.GovernanceToken_evt_DelegateChanged del
+        from delegate_change_for_claimers del
         cross join start_claim sc
         where del.evt_block_time >= sc.start_claim_tm
     )
     where rank = 1
 )
 
-, Delegate_BeforeAfter_Airdrop as (
+, claimer_delegate_changes as (
     select 
         delegator
         , delegate_before_airdrop
         , delegate_after_airdrop
-    from Delegate_Before_Airdrop
-    full outer join Delegate_After_Airdrop
+    from claimer_delegate_before_airdrop
+    full outer join claimer_delegate_after_airdrop
         using (delegator)
 )
 
-select * from Delegate_BeforeAfter_Airdrop
-
-
-
-
+select * from claimer_delegate_changes
