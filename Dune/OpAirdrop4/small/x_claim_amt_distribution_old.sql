31d0ed8360031f0d48c6381b202452fc4f72c35a
stasi <stasi009@163.com>
2024-04-06 08:46:16 +0800

    save

--- Dune/OpAirdrop4/small/x_claim_amt_distribution_old.sql
+++ Dune/OpAirdrop4/small/x_claim_amt_distribution_old.sql
@@ -10,3 +10,31 @@ with evt_claimed as (
         and contract_address = 0xFb4D5A94b516DF77Fbdbcf3CfeB262baAF7D4dB7 -- airdrop contract
         and topic0 = 0x4ec90e965519d92681267467f775ada5bd214aa92c0dc93d90a5e880ce9ed026 -- claimed
 )
+
+, opt_amt_range as (
+    select 
+        min(op_amt_adjdec) as min_amt
+        , (max(op_amt_adjdec) - min(op_amt_adjdec))/{{bins}} as bin_width
+    from evt_claimed
+)
+
+, op_amt_bins as (
+    select 
+        idx
+        , r.min_amt + (idx-1)*r.bin_width as lb
+        , r.min_amt + idx * r.bin_width as ub
+    from unnest(sequence(1,{{bins}})) as tbl(idx)
+    cross join opt_amt_range r
+)
+
+select 
+    b.idx as bin_idx
+    , round(b.lb,2) as bin_lb
+    , count(c.claimer) as num_claimers
+from op_amt_bins b
+left join evt_claimed c
+    on c.op_amt_adjdec >= b.lb 
+    and c.op_amt_adjdec < b.ub
+group by 1,2
+order by 1
+
