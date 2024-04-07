1e05d155375aee8484d5a03d963610b4de144c97
stasi <stasi009@163.com>
2024-04-07 11:23:33 +0800

    save

--- Dune/OpAirdrop4/small/temp.sql
+++ Dune/OpAirdrop4/small/temp.sql
@@ -1,34 +1,10 @@
-with vote_power_decrease as (
-    select 
-        block_time
-        , tx_hash
-        , delegate
-        , power_diff
-    from op_governance_optimism.delegates
-    where power_diff < 0
-)
-, op_transfer as (
-    select 
-        evt_tx_hash
-        , tf."from"
-        , cast(tf.value as double)/1e18 as op_amount
-    from erc20_optimism.evt_transfer tf
-    left join op_governance_optimism.delegates_addresses deladdr
-        on tf."from" = deladdr.address
-    where deladdr.address is null  -- 不是由delegate向外发出
-        and tf."from" <> 0x0000000000000000000000000000000000000000
-)
-
 select
-    vpd.block_time
-    , vpd.tx_hash
-    , tf."from" as delegator
-    , vpd.delegate
-    , vpd.power_diff
-    , tf.op_amount as transfer_op
-from vote_power_decrease vpd 
-inner join op_transfer tf 
-    on vpd.tx_hash = tf.evt_tx_hash
-where vpd.block_time >= date '2024-04-01'
-order by transfer_op desc 
-limit 100
+    evt_block_time as block_time 
+    , evt_tx_hash
+    , "from"
+    , to
+    , if(to = 0xe60ead0c469e9801c886ed1080524fe9ab76bdff,1,-1) * cast(tf.value as double)/1e18 as op_amount
+from erc20_optimism.evt_transfer tf
+where contract_address = 0x4200000000000000000000000000000000000042 -- OP Token
+    and ("from" = 0xe60ead0c469e9801c886ed1080524fe9ab76bdff or to = 0xe60ead0c469e9801c886ed1080524fe9ab76bdff)
+order by 1
