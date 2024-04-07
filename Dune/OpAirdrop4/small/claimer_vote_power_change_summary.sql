797b79b1428b711f777ec9704a417a66a99e6c74
stasi <stasi009@163.com>
2024-04-07 16:20:10 +0800

    save

---
+++ Dune/OpAirdrop4/small/claimer_vote_power_change_summary.sql
@@ -0,0 +1,12 @@
+
+select 
+    change_summary
+    , count(claimer) as num_claimers
+    , sum(claim_op) as claim_op
+
+    , sum(vote_power_pre_ad) as vote_power_pre_ad
+    , sum(vote_power_post_ad) as vote_power_post_ad
+    , sum(vote_power_post_ad) - sum(vote_power_pre_ad) as vote_power_change
+from query_3598102 -- claimer_vote_power_change.sql
+group by change_summary
+
