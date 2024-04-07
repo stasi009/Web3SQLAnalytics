1de8e3096b47e74b7af047de3565664788322dd6
stasi <stasi009@163.com>
2024-04-07 14:24:27 +0800

    save

--- Dune/OpAirdrop4/small/s_claimer_change_after_airdrop.sql
+++ Dune/OpAirdrop4/small/s_claimer_change_after_airdrop.sql
@@ -131,3 +131,31 @@ from (
     left join claimer_op_current afo 
         on ac.claimer = afo.account 
 )
+
+, claimer_delegate_changes as (
+    select 
+        *  
+        -- !这里没有使用abs，尽管由于浮点数计算误差，有的account balance是-6e-16这个级别的负数
+        -- 但是考虑到这种极小的负数也能够被<1e-6处理，所以这里也就没使用abs
+        , case 
+            when vote_power_pre_ad <= 1e-6 and vote_power_post_ad > 1e-6
+                then 'Begin Delegate'
+            when vote_power_pre_ad > 1e-6 and vote_power_post_ad <= 1e-6
+                then 'Quit Delegate'
+            when vote_power_pre_ad <= 1e-6 and vote_power_post_ad <= 1e-6
+                then 'Still Not Delegate'
+            when delegate_before_airdrop = delegate_after_airdrop 
+                then 'Keep Same Delegate'
+            when delegate_before_airdrop <> delegate_after_airdrop 
+                then 'Change Delegate'  
+        end as change_after_airdrop
+    from query_3598102
+)
+
+select 
+    change_after_airdrop
+    , count(claimer) as num_claimers
+    , cast(count(claimer) as double) / (sum(count(claimer)) over () ) as claimer_percentage 
+from claimer_delegate_changes
+group by change_after_airdrop
+order by num_claimers desc
