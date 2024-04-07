0d1808388260b2f3127372bd431e6017ea76a167
stasi <stasi009@163.com>
2024-04-07 16:26:56 +0800

    save

---
+++ Dune/OpAirdrop4/small/claimer_vote_power_change_by_claimop.sql
@@ -0,0 +1,14 @@
+-- 在claimed op的不同区间，change summary的分布
+
+
+select 
+    change_summary
+    , count(claimer) as num_claimers
+    , sum(claim_op) as claim_op
+
+    , sum(vote_power_pre_ad) as vote_power_pre_ad
+    , sum(vote_power_post_ad) as vote_power_post_ad
+    , sum(vote_power_post_ad) - sum(vote_power_pre_ad) as vote_power_change
+from query_3598102 -- claimer_vote_power_change.sql
+group by change_summary
+
