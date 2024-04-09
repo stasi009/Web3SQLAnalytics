f8dda0620544b2db0068ffe4b22ec18a4fda0fbf
stasi <stasi009@163.com>
2024-04-09 11:05:54 +0800

    save

--- Dune/OpAirdrop4/medium/between_claimer_transfer_neighbors.sql
+++ Dune/OpAirdrop4/medium/between_claimer_transfer_neighbors.sql
@@ -1,34 +1,34 @@
--- https://dune.com/queries/3603698
-with between_claimers_transfers as (
-    select * from "query_3603670(backdays='365',blockchain='ethereum')" -- between_claimer_transfer
-    union all
-    select * from "query_3603670(backdays='365',blockchain='optimism')"
-    union all
-    select * from "query_3603670(backdays='365',blockchain='zora')"
-    union all
-    select * from "query_3603670(backdays='365',blockchain='base')"
-)
-
-, pairs as (
+-- https://dune.com/queries/3603775
+with pairs as (
     select 
         account1 
         , account2 
-        , count(1) as num_transfers
-    from between_claimers_transfers
-    group by 1,2 
-    having count(1) > 3
+        , num_transfers
+    from query_3603698 -- between_claimer_transfer_pairs.sql, https://dune.com/queries/3603698
+    where num_transfers > 3
 )
 
 select 
     center 
     , count(neighbor) as num_neighbors
+    , sum(num_transfers) as total_transfers
 from (
-    select account1 as center, account2 as neighbor from pairs 
+    select 
+        account1 as center
+        , account2 as neighbor 
+        , num_transfers
+    from pairs
+
     union all 
-    select account2 as center, account1 as neighbor from pairs
+    
+    select 
+        account2 as center
+        , account1 as neighbor 
+        , num_transfers
+    from pairs
 )
 group by 1 
-order by num_neighbors desc
+order by total_transfers desc
 
 
 
