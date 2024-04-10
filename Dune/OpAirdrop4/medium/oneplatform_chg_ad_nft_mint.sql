9b11dfaa9a47225fb4454b920554b62c11e76f37
stasi <stasi009@163.com>
2024-04-10 10:44:49 +0800

    save

--- Dune/OpAirdrop4/medium/oneplatform_chg_ad_nft_mint.sql
+++ Dune/OpAirdrop4/medium/oneplatform_chg_ad_nft_mint.sql
@@ -9,75 +9,44 @@ with days_since_announce_ad as (
         block_date
         , block_date < date '2024-02-21' as is_before_ad -- flag whether before announce airdrop
 
-        , blockchain
-        , project_contract_address
-        , project
-
         , count(tx_hash) as num_txns
         , count(distinct buyer) as num_minters -- buyers are mint to recipient
         , sum(amount_usd) as mint_cost_usd
     from nft.mints
     cross join days_since_announce_ad dsan
-    where blockchain in ('optimism', 'ethereum', 'zora', 'base')
+    where blockchain = '{{blockchain}}'
+        and project_contract_address = {{project_contract_address}}
         -- 今天距离announce过去多少天，就自announce向前回溯多少天
         and block_time >= date_add('day', -1*dsan.days, date '2024-02-21') 
         and block_time < current_date -- avoid incomplete date
-        and nft_contract_address <> project_contract_address --not native mint,必须是通过nft market来mint的
-    group by 1,2,3,4,5
+    group by 1,2
 )
 
-, each_project_mint_across_ad as (
+, medians as (
     select 
-        blockchain
-        , project_contract_address
-        , project
-
-        , count(block_date) filter (where is_before_ad) as pread_days
-        , count(block_date) filter (where not is_before_ad) as postad_days
-        
-        , approx_percentile(num_minters, 0.5) filter (where is_before_ad) as pread_med_minters
-        , approx_percentile(num_minters, 0.5) filter (where not is_before_ad) as postad_med_minters
-
-        , approx_percentile(num_txns, 0.5) filter (where is_before_ad) as pread_med_txns
-        , approx_percentile(num_txns, 0.5) filter (where not is_before_ad) as postad_med_txns
-
-        , approx_percentile(mint_cost_usd, 0.5) filter (where is_before_ad) as pread_med_mint_usd
-        , approx_percentile(mint_cost_usd, 0.5) filter (where not is_before_ad) as postad_med_mint_usd
-
+        is_before_ad
+        , approx_percentile(num_minters, 0.5) as med_minters
+        , approx_percentile(num_txns, 0.5) as med_txns
+        , approx_percentile(mint_cost_usd, 0.5) as med_mint_usd
     from daily_nft_mint
-    group by 1,2,3
+    group by 1
 )
 
 select 
-    blockchain
-    , project_contract_address
-    , get_href(get_chain_explorer_address(blockchain, project_contract_address),project) as project
+    is_before_ad
+    , dmt.block_date 
 
-    , pread_days
-    , postad_days
+    , dmt.num_txns
+    , md.med_txns
 
-    , pread_med_minters
-    , postad_med_minters
-    , cast(postad_med_minters as double)/pread_med_minters - 1 as minters_chg_rate
-    
-    , pread_med_txns
-    , postad_med_txns
-    , cast(postad_med_txns as double)/pread_med_txns - 1 as mint_txn_chg_rate
+    , dmt.num_minters 
+    , md.med_minters
 
-    , pread_med_mint_usd
-    , postad_med_mint_usd
-    , postad_med_mint_usd/pread_med_mint_usd - 1 as mint_usd_chg_rate
+    , dmt.mint_cost_usd
+    , md.med_mint_usd
 
-from each_project_mint_across_ad
-where pread_days >= 30
-    and postad_days >= 30
-    
-    and pread_med_minters > 10
-    and pread_med_txns > 10
-    and pread_med_mint_usd > 0
-    
-    and postad_med_minters > 0
-    and postad_med_txns > 0
-    and postad_med_mint_usd > 0
+from daily_nft_mint dmt
+inner join medians md
+    using (is_before_ad)
 
-order by mint_usd_chg_rate desc
+order by block_date
