2f166cc42cb97c8eae47a8300ee07f8237af7265
stasi <stasi009@163.com>
2024-04-12 20:25:11 +0800

    save

--- Dune/OpAirdrop4/large/gov_concentrate_ad_contrast.sql
+++ Dune/OpAirdrop4/large/gov_concentrate_ad_contrast.sql
@@ -1,12 +1,13 @@
-
+-- https://dune.com/queries/3616641
+-- airdrop前后，集中度变化对比
 select 
     'before_airdrop' as period
     , *   
-from  "query_3615639(mode='before_airdrop',topn_delegates='100')"
+from  "query_3615639(mode='before_airdrop',topn_delegates='{{topn_delegates}}')"
 
 union all 
 
 select 
     'current' as period
     , *   
-from  "query_3615639(mode='current',topn_delegates='100')"
+from  "query_3615639(mode='current',topn_delegates='{{topn_delegates}}')"
